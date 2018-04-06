bd5624eace967e823aa2ed019f0bed34858dcd82
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2018-04-06 02:51:58 +0000

    Initial structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# RT EQcorrscan
+## Real-time implementation of EQcorrscan's matched-filter earthquake detection
+
+Holding ground for the project.
+
+*Work in progress*
+
+# Contributing
+
+At the moment the project is in its infancy, so any help on project direction is
+appreciated, but bear with me while I build the barebones.
+
+# Funding
+
+This project is funded by the Earthquake Commission of New Zealand (EQC).
---
+++ setup.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+try:
+    # use setuptools if we can
+    from setuptools import setup, Command, Extension
+    from setuptools.command.build_ext import build_ext
+    using_setuptools = True
+except ImportError:
+    from distutils.core import setup, Command, Extension
+    from distutils.command.build_ext import build_ext
+    using_setuptools = False
+
+import glob
+import os
+import sys
+import shutil
+import rt_eqcorrscan
+
+VERSION = rt_eqcorrscan.__version__
+
+long_description = '''
+Real-time EQcorrscan: Real-time wrappers for EQcorrscan's earthquake detection
+methods.
+'''
+
+scriptfiles = glob.glob("rt_eqcorrscan/scripts/*")
+
+
+def setup_package():
+
+    # Figure out whether to add ``*_requires = ['numpy']``.
+    build_requires = []
+    try:
+        import numpy
+    except ImportError:
+        build_requires = ['numpy>=1.6, <2.0']
+
+    install_requires = ['matplotlib>=1.3.0', 'scipy>=0.18', 'LatLon',
+                        'bottleneck', 'obspy>=1.0.3', 'numpy>=1.12',
+                        'h5py', "eqcorrscan>=0.3.0"]
+    install_requires.extend(build_requires)
+
+    setup_args = {
+        'name': 'RT-EQcorrscan',
+        'version': VERSION,
+        'description': 'RT-EQcorrscan - Real-time matched-filter detection',
+        'long_description': long_description,
+        'url': 'https://github.com/calum-chamberlain/EQcorrscan',
+        'author': 'Calum Chamberlain',
+        'author_email': 'calum.chamberlain@vuw.ac.nz',
+        'license': 'LGPL',
+        'classifiers': [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering',
+            'License :: OSI Approved :: GNU Library or Lesser General Public '
+            'License (LGPL)',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+        'keywords': 'real-time earthquake correlation detection match-filter',
+        'scripts': scriptfiles,
+        'install_requires': install_requires,
+        'setup_requires': ['pytest-runner'],
+        'tests_require': ['pytest>=2.0.0', 'pytest-cov', 'pytest-pep8',
+                          'pytest-xdist', 'pytest-rerunfailures',
+                          'obspy>=1.1.0'],
+    }
+
+    if using_setuptools:
+        setup_args['setup_requires'] = build_requires
+        setup_args['install_requires'] = install_requires
+
+    if len(sys.argv) >= 2 and (
+        '--help' in sys.argv[1:] or
+        sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+                        'clean')):
+        # For these actions, NumPy is not required.
+        pass
+    else:
+        setup_args['packages'] = ['rt_eqcorrscan', 'rt_eqcorrscan.utils',
+                                  'rt_eqcorrscan.core']
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
+    setup(**setup_args)
+
+
+if __name__ == '__main__':
+    setup_package()
