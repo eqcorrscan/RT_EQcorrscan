d0f8e3dd239ffd783157f803f0e2444795d8ba71
calum-chamberlain <calum.chamberlain@vuw.ac.nz>
2024-08-08 08:16:59 +1200

    Work on testing growclust runner

---
+++ rt_eqcorrscan/plugins/relocation/tests/test_growclust_runner.py
@@ -0,0 +1,135 @@
+"""
+Test for groweclust runner
+"""
+
+import unittest
+import logging
+import time
+import os
+import shutil
+
+from typing import List
+
+from obspy import Catalog, UTCDateTime
+
+from eqcorrscan.utils.catalog_utils import filter_picks
+
+from rt_eqcorrscan.plugins.relocation.growclust_runner import main
+
+Logger = logging.getLogger(__name__)
+
+
+def _write_detections_for_sim(
+    catalog: Catalog,
+    outdir: str,
+    poison_dir: str,
+    sleep_step: float = 20.0,
+):
+    slices = [slice(0, 1), slice(1, 5), slice(5, None)]
+    for _slice in slices:
+        events = catalog[_slice]
+        Logger.info(events)
+        for event in events:
+            Logger.debug(f"Writing event {event.resource_id.id.split('/')[-1]}")
+            event.write(f"{outdir}/{event.resource_id.id.split('/')[-1]}.xml",
+                        format="QUAKEML")
+        time.sleep(sleep_step)
+    Logger.info("Writing poison file")
+    with open(f"{poison_dir}/poison", "w") as f:
+        f.write("Poisoned at end of detection writing")
+    return
+
+
+def _get_bulk_for_cat(cat: Catalog) -> List[tuple]:
+    picks = sorted([p for ev in cat for p in ev.picks], key=lambda p: p.time)
+    starttime = picks[0].time
+    endtime = picks[-1].time
+    bulk = {
+        (p.waveform_id.network_code or "*",
+         p.waveform_id.station_code or "*",
+         p.waveform_id.location_code or "*",
+         p.waveform_id.channel_code or "*")
+        for p in picks}
+    bulk = [(n, s, l, c, starttime, endtime) for n, s, l, c in bulk]
+    return bulk
+
+
+def setup_testcase(
+    wavedir: str,
+    eventdir: str,
+):
+    """
+    Download some event files and a stationxml to run.
+    """
+    from obspy.clients.fdsn import Client
+
+    client = Client("GEONET")
+
+    Logger.info("Setting up testcase")
+    # Kaikōura - Cape Campbell ~ 200 events
+    cat = client.get_events(
+        starttime=UTCDateTime(2016, 11, 13),
+        endtime=UTCDateTime(2016, 11, 14),
+        minlatitude=-41.9,
+        maxlatitude=-41.6,
+        minlongitude=174.0,
+        maxlongitude=174.4,
+        maxdepth=40
+    )
+    Logger.info(f"Downloaded {len(cat)} events")
+
+    # Just use the top n most picked stations
+    cat = filter_picks(cat, top_n_picks=15)
+    cat.events = [ev for ev in cat if len(ev.picks)]
+
+    bulk = _get_bulk_for_cat(cat=cat)
+    inv = client.get_stations_bulk(bulk, level="channel")
+
+    st = client.get_waveforms_bulk(bulk)
+    st = st.merge()
+
+    inv.write(f"{eventdir}/stations.xml", format="STATIONXML")
+    if not os.path.isdir(wavedir):
+        os.makedirs(wavedir)
+    if not os.path.isdir(eventdir):
+        os.makedirs(eventdir)
+
+    for ev in cat:
+        ev.write(f"{eventdir}/{ev.resource_id.id.split('/')[-1]}.xml",
+                 format="QUAKEML")
+
+    for tr in st:
+        tr.split().write(f"{wavedir}/{tr.id}.ms", format="MSEED")
+
+    Logger.info("Completed test set-up")
+    return cat
+
+
+class TestGrowclustPlugin(unittest.TestCase):
+    clean_up = []
+    @classmethod
+    def setUpClass(cls):
+        cls.eventdir = "growclust_test_events"
+        cls.wavedir = "growclust_test_streams"
+        cls.cat = setup_testcase(
+            eventdir=cls.eventdir,
+            wavedir=cls.wavedir
+        )
+        cls.clean_up.append(cls.stations_file)
+
+    def test_runner(self):
+        self.assertTrue(False)
+
+    @classmethod
+    def tearDownClass(cls, clean=True):
+        if clean:
+            for thing in cls.clean_up:
+                if os.path.isdir(thing):
+                    shutil.rmtree(thing)
+                else:
+                    os.remove(thing)
+            _cleanup()
+
+
+if __name__ == "__main__":
+    unittest.main()
