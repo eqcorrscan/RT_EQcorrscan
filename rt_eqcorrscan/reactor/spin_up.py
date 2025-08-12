43635c638da2e44c26cb7bb7622cbc618db5ed20
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2025-08-13 11:29:29 +1200

    plug typo

--- rt_eqcorrscan/reactor/spin_up.py
+++ rt_eqcorrscan/reactor/spin_up.py
@@ -99,7 +99,7 @@ def run(
             # We need to handle the stationxml file and velocity file here
             config.plugins[plug].station_file = os.path.join(
                 working_dir, "inventory.xml")
-    for plug in ["plotter", "outputter"]:
+    for plug in ["plotter", "output"]:
         if config.plugins[plug]:
             config.plugins[plug].mainshock_id = triggering_event.resource_id.id.split('/')[-1]
     detect_interval = config.rt_match_filter.get(
